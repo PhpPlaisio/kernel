7bbb7966f212c4d9c869d925a6180c4f60eebbe3
Paul Water <p.r.water@setbased.nl>
2017-04-03 15:20:43 +0200

    Set encoding for unit tests.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,12 +1,6 @@
 <?php
-
-use SetBased\Affirm\ErrorHandler;
-
+mb_internal_encoding('UTF-8');
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Amsterdam');
 
 require __DIR__.'/../vendor/autoload.php';
-
-// Set the error handler.
-$handler = new ErrorHandler();
-$handler->register(false);
