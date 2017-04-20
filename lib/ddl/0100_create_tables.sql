cb99ec005482a7a7b9953bdddbcda3f313f2f671
Paul Water <p.r.water@setbased.nl>
2017-04-20 15:29:44 +0200

    Moved request log tables to package abc-request-logger-light.

--- lib/ddl/0100_create_tables.sql
+++ lib/ddl/0100_create_tables.sql
@@ -5,7 +5,7 @@
 /*  FileName : abc.ecm                                                            */
 /*  Platform : MySQL 5                                                            */
 /*  Version  : Concept                                                            */
-/*  Date     : zondag 9 april 2017                                                */
+/*  Date     : donderdag 20 april 2017                                            */
 /*================================================================================*/
 /*================================================================================*/
 /* CREATE TABLES                                                                  */
@@ -423,47 +423,6 @@ CREATE TABLE `LOG_LOGIN` (
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`lli_id`)
 );
 
-CREATE TABLE `LOG_REQUEST` (
-  `rql_id` INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
-  `cmp_id` SMALLINT UNSIGNED NOT NULL,
-  `pag_id` SMALLINT UNSIGNED,
-  `ses_id` INTEGER UNSIGNED,
-  `usr_id` INTEGER UNSIGNED NOT NULL,
-  `rql_datetime` DATETIME NOT NULL,
-  `rql_request` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rql_method` VARCHAR(8) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rql_referer` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rql_ip` INT UNSIGNED,
-  `rql_host_name` VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rql_language` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rql_user_agent` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rql_number_of_queries` INT,
-  `rql_time` FLOAT,
-  `rql_size` INT,
-  CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`rql_id`)
-);
-
-CREATE TABLE `LOG_REQUEST_COOKIE` (
-  `rql_id` INTEGER UNSIGNED NOT NULL,
-  `rcl_variable` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `rcl_value` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci
-);
-
-CREATE TABLE `LOG_REQUEST_POST` (
-  `rql_id` INTEGER UNSIGNED NOT NULL,
-  `rlp_variable` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `rlp_value` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci
-);
-
-CREATE TABLE `LOG_REQUEST_QUERY` (
-  `rqq_id` INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
-  `rql_id` INTEGER UNSIGNED NOT NULL,
-  `rqq_query` MEDIUMBLOB NOT NULL,
-  `rqq_time` FLOAT NOT NULL,
-  `rqq_routine` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`rqq_id`)
-);
-
 /*================================================================================*/
 /* CREATE INDEXES                                                                 */
 /*================================================================================*/
@@ -575,20 +534,6 @@ CREATE INDEX `llg_user_name` ON `LOG_LOGIN` (`llg_user_name`);
 CREATE INDEX `ses_id` ON `LOG_LOGIN` (`ses_id`);
 
 CREATE INDEX `usr_id` ON `LOG_LOGIN` (`usr_id`);
-
-CREATE INDEX `cmp_id` ON `LOG_REQUEST` (`cmp_id`);
-
-CREATE INDEX `IX_LOG_REQUEST1` ON `LOG_REQUEST` (`ses_id`);
-
-CREATE INDEX `pag_id` ON `LOG_REQUEST` (`pag_id`);
-
-CREATE INDEX `usr_id` ON `LOG_REQUEST` (`usr_id`);
-
-CREATE INDEX `rql_id` ON `LOG_REQUEST_COOKIE` (`rql_id`);
-
-CREATE INDEX `rql_id` ON `LOG_REQUEST_POST` (`rql_id`);
-
-CREATE INDEX `rql_id` ON `LOG_REQUEST_QUERY` (`rql_id`);
 
 /*================================================================================*/
 /* CREATE FOREIGN KEYS                                                            */
@@ -870,15 +815,3 @@ ALTER TABLE `LOG_LOGIN`
   FOREIGN KEY (`usr_id`) REFERENCES `AUT_USER` (`usr_id`)
   ON UPDATE NO ACTION
   ON DELETE NO ACTION;
-
-ALTER TABLE `LOG_REQUEST_COOKIE`
-  ADD CONSTRAINT `FK_LOG_REQUEST_COOKIE_LOG_REQUEST`
-  FOREIGN KEY (`rql_id`) REFERENCES `LOG_REQUEST` (`rql_id`);
-
-ALTER TABLE `LOG_REQUEST_POST`
-  ADD CONSTRAINT `FK_LOG_REQUEST_POST_LOG_REQUEST`
-  FOREIGN KEY (`rql_id`) REFERENCES `LOG_REQUEST` (`rql_id`);
-
-ALTER TABLE `LOG_REQUEST_QUERY`
-  ADD CONSTRAINT `FK_LOG_REQUEST_QUERY_LOG_REQUEST`
-  FOREIGN KEY (`rql_id`) REFERENCES `LOG_REQUEST` (`rql_id`);
